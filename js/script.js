d61f7492f2c12c229946478ed48070594ec9c2a6
VictoriaKwamboka <victoriamonari9815@gmail.com>
2022-03-18 08:48:01 +0300

    Add the a user interface logic

--- js/script.js
+++ js/script.js
@@ -1,4 +1,32 @@
+
+//business logic
 function Contact(first, last){
     this.firstName = first;
     this.lastName = last
 }
+
+//create a jQuery callback to collect user input and assign it to variables
+// use the user input to create new Contact objects
+// append the new objects to the list
+
+//user interface logic
+$(document).ready(function(){
+    $('form#new-contact').submit(function(event){
+        event.preventDefault();
+
+        let inputtedFirstName = $('input#new-first-name').val()
+        let inputtedLastName = $('input#new-last-name').val()
+
+        let newContact = new Contact(inputtedFirstName,inputtedLastName)
+
+        $('ul#contacts').append('<li><span class= "contact">' + newContact.firstName + '</span></li>')
+    });
+    //show the contact information when the contact is clicked
+
+    $('.contact').last().click(function(){
+        $('#show-contact').show()
+        // $('#show contact h2').text(newContact.firstName);
+        // $('.first-name').text(newContact.firstName)
+        // $('.last-name').text(newContact.lastName)
+    })
+})
